85950ba307b983b108a515940eaff916533a0543
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-04-25 10:00:29 +0300

    Refactor login screen

--- lib/views/authentication/signup_screen.dart
+++ lib/views/authentication/signup_screen.dart
@@ -14,117 +14,122 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Container(
-            padding: kPageContentPadding,
-            child: Column(
-              children: [
-                const CustomHeader(title: "Sign Up"),
-                Container(
-                  decoration: kContenetCardDecoration,
-                  //padding: kContentCardPadding,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 5),
-                        child: const Text(
-                          "Create Account",
-                          style: kHeaderTextStyle,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 25),
-                        child: const Text(
-                          'sign up to create a new account',
-                          style: kNormalTextStyle,
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
-                        child: CustomTextFormFiled(
-                          hintText: "E-mail",
-                          prefixIcon: const Icon(
-                            Icons.email_outlined,
-                            color: kPrimaryColor,
+        child: Container(
+          padding: kPageContentPadding,
+          child: Column(
+            children: [
+              const CustomHeader(title: "Sign Up"),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Container(
+                    decoration: kContenetCardDecoration,
+                    padding: kContentCardPadding,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 5),
+                          child: const Text(
+                            "Create Account",
+                            style: kHeaderTextStyle,
                           ),
-                          keyboardType: TextInputType.emailAddress,
-                          onSaved: (value) {},
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 10),
-                        child: CustomTextFormFiled(
-                          hintText: "Password",
-                          prefixIcon: const Icon(
-                            Icons.lock_open,
-                            color: kPrimaryColor,
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 25),
+                          child: const Text(
+                            'sign up to create a new account',
+                            style: kNormalTextStyle,
                           ),
-                          onSaved: (value) {},
                         ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Row(
-                          children: [
-                            CustomCheckbox(
-                              labelWidget: Padding(
-                                padding: const EdgeInsets.only(top: 10.0),
-                                child: RichText(
-                                  textAlign: TextAlign.left,
-                                  text: TextSpan(
-                                    style: kNormalTextStyle,
-                                    children: [
-                                      const TextSpan(
-                                        text:
-                                            'By clicking this, I agree to the ',
-                                      ),
-                                      TextSpan(
-                                        text:
-                                            'Terms & Conditions & Privacy Policy',
-                                        style: kHyperLinkTextStyle,
-                                        recognizer: TapGestureRecognizer()
-                                          ..onTap = () {},
-                                      ),
-                                    ],
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 15),
+                          child: CustomTextFormFiled(
+                            hintText: "E-mail",
+                            prefixIcon: const Icon(
+                              Icons.email_outlined,
+                              color: kPrimaryColor,
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                            onSaved: (value) {},
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 10),
+                          child: CustomTextFormFiled(
+                            hintText: "Password",
+                            prefixIcon: const Icon(
+                              Icons.lock_open,
+                              color: kPrimaryColor,
+                            ),
+                            onSaved: (value) {},
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 20),
+                          child: Row(
+                            children: [
+                              CustomCheckbox(
+                                labelWidget: Padding(
+                                  padding: const EdgeInsets.only(top: 10.0),
+                                  child: RichText(
+                                    textAlign: TextAlign.left,
+                                    text: TextSpan(
+                                      style: kNormalTextStyle,
+                                      children: [
+                                        const TextSpan(
+                                          text:
+                                              'By clicking this, I agree to the ',
+                                        ),
+                                        TextSpan(
+                                          text:
+                                              'Terms & Conditions & Privacy Policy',
+                                          style: kHyperLinkTextStyle,
+                                          recognizer: TapGestureRecognizer()
+                                            ..onTap = () {},
+                                        ),
+                                      ],
+                                    ),
                                   ),
                                 ),
+                                value: false,
+                                onChanged: (value) {},
                               ),
-                              value: false,
-                              onChanged: (value) {},
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      RoundedButton(
-                        text: 'Sign Up',
-                        width: MediaQuery.of(context).size.width,
-                        backgroundColor: kPrimaryColor,
-                        onPressed: () {},
-                      )
-                    ],
+                        RoundedButton(
+                          text: 'Sign Up',
+                          width: MediaQuery.of(context).size.width,
+                          backgroundColor: kPrimaryColor,
+                          onPressed: () {},
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12),
+                          child: RichText(
+                            textAlign: TextAlign.center,
+                            text: TextSpan(
+                              children: [
+                                const TextSpan(
+                                  text: 'Already have an account? ',
+                                  style: kNormalTextStyle,
+                                ),
+                                TextSpan(
+                                  text: 'Sign In',
+                                  style: kHyperLinkTextStyle,
+                                  recognizer: TapGestureRecognizer()
+                                    ..onTap = () {
+                                      Navigator.pushNamed(context, '/login');
+                                    },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                RichText(
-                  textAlign: TextAlign.center,
-                  text: TextSpan(
-                    children: [
-                      const TextSpan(
-                        text: 'Already have an account? ',
-                        style: kNormalTextStyle,
-                      ),
-                      TextSpan(
-                        text: 'Sign In',
-                        style: kHyperLinkTextStyle,
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.pushNamed(context, '/login');
-                          },
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
