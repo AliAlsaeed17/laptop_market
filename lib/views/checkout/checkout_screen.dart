8427579d9f8416ae6e5d490bb54b226a1f658246
AliAlsaeed176 <alialsaeed176@gmail.com>
2022-06-16 20:31:35 +0300

    Refactor and update UI

--- lib/views/checkout/checkout_screen.dart
+++ lib/views/checkout/checkout_screen.dart
@@ -13,14 +13,15 @@ class CheckOutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
       //backgroundColor: kBackgroundColor,
       bottomNavigationBar: CustomNavigationBar(
         widgets: Padding(
-          padding: kBottomNavigationBarPadding.copyWith(top: 25, bottom: 15),
+          padding: kBottomNavigationBarPadding,
           child: RoundedButton(
             text: 'Go to Payment',
-            height: 20,
             onPressed: () {
               Get.toNamed('/payment');
             },
@@ -30,108 +31,143 @@ class CheckOutScreen extends StatelessWidget {
       ),
       body: SafeArea(
         child: Container(
+          width: width,
+          height: height,
           padding: kPageContentPadding,
           child: Column(
             children: [
-              const CustomHeader(title: 'Check Out'),
-              const SizedBox(height: 20),
+              Padding(
+                padding: EdgeInsets.only(bottom: height * .04),
+                child: CustomHeader(title: 'Check Out'),
+              ),
               Expanded(
                 child: Container(
                   decoration: kContenetCardDecoration.copyWith(
-                      color: Theme.of(context).backgroundColor),
-                  padding:
-                      EdgeInsets.symmetric(horizontal: 10.0, vertical: 10.0),
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                  padding: kContentCardPadding,
                   child: SingleChildScrollView(
                     child: Column(
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text(
-                              'Shipping Address',
-                              style: kSubHeaderTextStyle,
-                            ),
-                            RoundedButton(
-                              text: 'Edit',
-                              radius: 5,
-                              width: 70,
-                              height: 32,
-                              textStyle: const TextStyle(color: kPrimaryColor),
-                              backgroundColor: kCardBackgroundColor,
-                              onPressed: () {},
-                            ),
-                          ],
+                        Padding(
+                          padding: EdgeInsets.only(bottom: height * .02),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(bottom: height * .03),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      'Shipping Address',
+                                      style: kSubHeaderTextStyle,
+                                    ),
+                                    RoundedButton(
+                                      text: 'Edit',
+                                      radius: 5,
+                                      width: width * .1,
+                                      height: height * .045,
+                                      textStyle:
+                                          const TextStyle(color: kPrimaryColor),
+                                      backgroundColor: kCardBackgroundColor,
+                                      onPressed: () {},
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              Row(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Padding(
+                                    padding:
+                                        EdgeInsets.only(right: width * .02),
+                                    child: IconCard(
+                                      icon: Icon(
+                                        Icons.location_on_outlined,
+                                        color: kPrimaryColor,
+                                      ),
+                                    ),
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            bottom: height * .01),
+                                        child: Text(
+                                          'Jelly Grande',
+                                          //TODO : need to config light/dark theme
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            bottom: height * .01),
+                                        child: Text(
+                                          '+62 123 456 78',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: EdgeInsets.only(
+                                            bottom: height * .01),
+                                        child: Text(
+                                          '871 kengan street(between jones Levanscwoth st) San Francisco',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .bodyMedium,
+                                        ),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                        const SizedBox(height: 10.0),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            const IconCard(
-                              icon: Icon(
-                                Icons.location_on_outlined,
-                                color: kPrimaryColor,
+                        Padding(
+                          padding: EdgeInsets.only(bottom: height * .01),
+                          child: Column(children: [
+                            Padding(
+                              padding: EdgeInsets.only(bottom: height * .022),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  const Text(
+                                    'Order Summary',
+                                    style: kSubHeaderTextStyle,
+                                  ),
+                                  RoundedButton(
+                                    text: 'Edit',
+                                    radius: 5,
+                                    width: width * .1,
+                                    height: height * .045,
+                                    textStyle:
+                                        const TextStyle(color: kPrimaryColor),
+                                    backgroundColor: kCardBackgroundColor,
+                                    onPressed: () {},
+                                  ),
+                                ],
                               ),
                             ),
-                            const SizedBox(width: 10),
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(
-                                  'Jelly Grande',
-                                  //TODO : need to config light/dark theme
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                                const SizedBox(height: 7.0),
-                                Text(
-                                  '+62 123 456 78',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                                const SizedBox(height: 7.0),
-                                // RichText(
-                                //   textAlign: TextAlign.left,
-                                //   text: const TextSpan(children: [
-                                //     TextSpan(
-                                //       text:
-                                //           '871 kengan street(between jones Levanscwoth st) San Francisco ',
-                                //     )
-                                //   ]),
-                                // )
-                                Text(
-                                  '871 kengan street(between jones Levanscwoth st) San Francisco',
-                                  style: Theme.of(context).textTheme.bodyMedium,
-                                ),
-                              ],
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 20.0),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            const Text(
-                              'Order Summary',
-                              style: kSubHeaderTextStyle,
+                            Container(
+                              height: height * .7,
+                              child: ListView.separated(
+                                itemBuilder: (context, index) =>
+                                    OrderProductCard(),
+                                separatorBuilder: (context, index) =>
+                                    SizedBox(height: height * .025),
+                                itemCount: 4,
+                              ),
                             ),
-                            RoundedButton(
-                              text: 'Edit',
-                              radius: 5,
-                              width: 70,
-                              height: 32,
-                              textStyle: const TextStyle(color: kPrimaryColor),
-                              backgroundColor: kCardBackgroundColor,
-                              onPressed: () {},
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 10.0),
-                        Container(
-                          height: 326,
-                          child: ListView.separated(
-                            itemBuilder: (context, index) => OrderProductCard(),
-                            separatorBuilder: (context, index) =>
-                                SizedBox(height: 12),
-                            itemCount: 4,
-                          ),
+                          ]),
                         ),
                       ],
                     ),
